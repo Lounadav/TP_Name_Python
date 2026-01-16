663862d84506dcfc903eae48390344b6a77d046a
Louna <lounadav1909@gmail.com>
2026-01-16 12:39:15 +0100

    Ajout soustraction, multiplication,division dans calcul.py

--- Exemples/calcul.py
+++ Exemples/calcul.py
@@ -1,13 +1,40 @@
 def main():
     print("Programme principal")
-def addition(a, b):
+def addition(a : int, b : int):
     """
     Docstring for addition
     
-    :param a: Description
-    :param b: Description
+    :param a: int
+    :param b: int
     """
     return a + b
+def soustraction(a : int, b : int):
+    """
+    Docstring for soustraction
+    
+    :param a: int
+    :param b: int
+    """
+    return a - b
+def multiplication(a : int, b : int):
+    """
+    Docstring for multiplication
+    
+    :param a: int
+    :param b: int
+    """
+    return a * b
+def division(a : int, b : int):
+    """
+    Docstring for division
+    
+    :param a: int
+    :param b: int
+    """
+    if b == 0:
+        raise ValueError("Division par 0 non autorisée")
+    else:
+        return a / b
 
 if __name__ == "__main__":
     main()
